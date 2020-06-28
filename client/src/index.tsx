2839234c914a4f96a518cb3eb00f4654dfd80553
gongchen <v_cgong@digitalgd.com.cn>
2020-06-29 00:25:28 +0800

    fix

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,18 +2,31 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { Switch, Route, Redirect } from 'react-router-dom'; // 路由组件
 import { Provider } from 'react-redux'; // 负责把属性中的store 传递给子组件
-// import store from  './store';  // 引入仓库
+import store from  './store';  // 引入仓库
 import { ConfigProvider } from 'antd'; // 配置
 import zh_CN from 'antd/lib/locale-provider/zh_CN'; // 国际化中文
 import './assets/css/common.less'; // 通用样式
 import Tabs from './components/Tabs' // 底部导航标签 
 import Home from './routes/Home'; // 首页
-import Prpfile from './routes/Mine'; // 我的课程
+import Mine from './routes/Mine'; // 我的课程
+import Profile from './routes/Profile'; // 我的课程
 import { ConnectedRouter } from 'connected-react-router' // redux 绑定路由
 import history from './store/history'
-// import {} 
-ReactDOM.render(
+ReactDom.render(
 	<Provider store={store}>
-	
-	</Provider>
-)
+		<ConnectedRouter history={history}>
+			<ConfigProvider locale={zh_CN}>
+				<main className="main-container">
+					<Switch>
+						<Route path="/" exact component={Home} />
+						<Route path="/mine" component={Mine} />
+						<Route path="/profile" component={Profile} />
+						<Redirect to="/" />
+					</Switch>
+				</main>
+				<Tabs />
+			</ConfigProvider>
+		</ConnectedRouter>
+	</Provider>,
+	document.getElementById("root")
+);
