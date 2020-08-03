dfb25f6d8da32943e7349f471dbdc90b7b7ee028
gongchen <gc12639179@gmail.com>
2020-08-04 01:32:31 +0800

    merger

--- client/src/routes/Profile/index.tsx
+++ client/src/routes/Profile/index.tsx
@@ -8,11 +8,7 @@ type Props = PropsWithChildren<RouteComponentProps<Params>>
 function Profile(props: Props) {
 	return (
     <div>
-<<<<<<< HEAD
-      profile    
-=======
 			demo
->>>>>>> e42c19a7b7f68252d08b29a52d35eaeb031f06de
     </div>
   );
 }
